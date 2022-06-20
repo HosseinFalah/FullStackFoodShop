ca290abf7c9c167735afe932b28fe10f5e8d0887
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2022-06-20 17:39:29 +0430

    add Reducer and State Management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StateProvider } from './Context/StateProvider';
+import { initialState } from './Context/initalState';
+import reducer from './Context/reducer';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
   <Router>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </Router>
 );
